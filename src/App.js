6447d91f7b85820992f37ca9757bc85468f288c5
scrumpygeo <scrumpygeo@yahoo.co.uk>
2023-01-04 12:43:46 +0000

    setup boilerplate/framework

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import { useState } from 'react';
+import Modal from './Modal'
+import { reducer } from './reducer'
 
 function App() {
+  const [name, setName] = useState('')
+  const [people, setPeople] = useState([
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Paul' },
+    { id: 3, name: 'George' },
+    { id: 4, name: 'Ringo' }
+  ])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
-    <div >
-      hello
+    <div className="container">
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input type="text" />
+        </div>
+        <button type="submit" className="btn">Add Person</button>
+      </form>
+      {people.map(person => {
+        return (
+          <div className="item" key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        )
+      })}
     </div>
+
+
   );
 }
 
